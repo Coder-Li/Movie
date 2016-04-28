7a577299cff3eb6181c18036724cd2600d2b08a9
Coder-Li <858582381@qq.com>
2016-04-28 21:17:39 +0800

    Login&Logup
    添加了注册和登录的前端页面，并且调整了grunt配置。添加了user模型！

---
+++ schemas/user.js
@@ -0,0 +1,51 @@
+//模式定义
+var mongoose = require('mongoose');
+//密码加密算法(windows 下不好安装)
+// var bcrypt = require('bcrypt');
+// 这里面放跟用户有过的字段和类型
+var UserSchema = new mongoose.Schema({
+    name: {
+        unique: true,
+        type: String
+    },
+    password:  String,
+    meta: {
+        createAt: {
+            type: Date,
+            default: Date.now()
+        },
+        updateAt: {
+            type: Date,
+            default: Date.now()
+        }
+    }  
+})
+//存数据前都会调用
+UserSchema.pre('save', function(next){
+    var user = this;
+    
+    if(this.isNew){
+        this.meta.createAt = this.meta.updateAt = Date.now();
+    }
+    else{
+        this.meta.updateAt = Date.now();
+    }
+    
+    user.password = salt;
+    next();
+});
+
+UserSchema.statics = {
+    fetch: function(cb){
+        return this
+            .find({})
+            .sort('meta.updateAt')
+            .exec(cb)
+    },
+    findById: function(id, cb){
+        return this
+            .findOne({_id: id})
+            .exec(cb)
+    }
+}
+module.exports = UserSchema
