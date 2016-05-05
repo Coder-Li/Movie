d92c275a6d81dd2faea691ae5a2994faadffc57e
Coder-Li <858582381@qq.com>
2016-05-05 21:26:21 +0800

    page
    分页

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -18,3 +18,39 @@ exports.index = function (req, res) {
         });
 
 }
+// search 
+exports.search = function (req, res) {
+    var catId = req.query.cat;
+    var page = parseInt(req.query.p, 10);
+    //每页条目数
+    var count = 2;
+    var index = page * count;
+    
+    Category
+        .find({_id: catId})
+        .populate({
+             path: 'movies', 
+             select: 'title poster',
+            //  options: { limit: 2, skip: index } 
+        })
+        .exec(function (err, categories) {
+
+            if (err) {
+                console.log(err);
+            }
+            
+            var category = categories[0] || {};
+            var movies = category.movies || [];
+            var results = movies.slice(index, index + count);
+            
+            res.render('results', {
+                title: '结果列表',
+                keyword: category.name,
+                currentPage: (page + 1),
+                movies: results,
+                totalPage: Math.ceil(movies.length / count),
+                query: 'cat=' + catId
+            });
+        });
+
+}
